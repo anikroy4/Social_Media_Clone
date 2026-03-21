3322b3e1ac179206563c1b0f0c7bf61f7fbeb93f
Anik Roy <anikroy.uiu@gmail.com>
2026-03-21 15:12:29 +0600

    password Length& Username Validation
    password Length& Username Validation

--- backend/helpers/validation.js
+++ backend/helpers/validation.js
@@ -6,3 +6,31 @@ exports.validateEmail = (email) => {
      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return String(email).toLowerCase().match(re);  
 }
+
+exports.validateLength =  (text, minLength, maxLength) => {
+    if (text.length < minLength || text.length > maxLength) {
+        return false;
+    }else{
+        return true;
+    }
+}
+
+
+exports.validateUsername = async(username)=>{
+    username = String(username).toLowerCase();
+    let isTrue=false;
+    do{
+        let user= await User.findOne({username})
+
+        if (user){
+            username = (username + (+new Date()+Math.random()).toString().substring(0, 1)).toLowerCase();
+            isTrue =true;
+        }else{
+            isTrue= false;
+        }
+
+
+    }while(isTrue);
+    
+        return username;
+}
